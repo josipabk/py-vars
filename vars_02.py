66ee57e11dbf0c74938995514b3540f7466f42b0
josipabk <josipabaljkas@gmail.com>
2025-09-01 20:51:06 +0200

    Update vars_02.py

--- vars_02.py
+++ vars_02.py
@@ -1,1 +1,19 @@
-print("Zivjeli!")
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) za:
+ - Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, težinu te spol
+ - Stranice a i b, četverokuta te za površinu tog četverokuta.
+'''
+
+# Osobni podaci
+ime = "Josipa"
+prezime = "Baljkas Klisović"
+godina_rodenja = 1981
+drzava_rodenja = "Hrvatska"
+status_radnog_odnosa = "zaposlena"
+tezina = 12
+spol = "žensko"
+
+# Četverokut
+a = 5
+b = 3
+povrsina = a * b
