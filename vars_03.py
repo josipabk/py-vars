21dd8f0c24e87ba19a6c0a1d3a93d70333692dc4
josipabk <josipabaljkas@gmail.com>
2025-09-03 18:45:23 +0200

    Create vars_03.py

---
+++ vars_03.py
@@ -0,0 +1,40 @@
+# Ovo je komentar
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjene proizvoljne dužine trokuta.
+'''
+a = 5
+b = 7
+c = 10
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o filmu: naziv filma, 
+godina proizvodnje, proječna ocjena, kratki opis
+'''
+naziv_filma = "Inception"
+godina_proizvodnje = 2010
+prosjecna_ocjena = 8.8
+kratki_opis = "Film o snovima unutar snova."
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o imenu i prezimenu studenta, 
+završnoj ocjeni te podataka je li diplomirao ili nije
+'''
+ime_prezime = "Ivan Horvat"
+zavrsna_ocjena = 4.5
+diplomirao = True
+
+'''
+Kreirati varijablu za čuvanje podataka o studentu: ime i prezime, OIB, email, 
+broj telefona, prosječna ocjena, godina upisa studija, naziv studija, naziv sveučilišta
+'''
+student = {
+    "ime_prezime": "Ana Anić",
+    "OIB": "12345678901",
+    "email": "ana.anic@gmail.com",
+    "telefon": "+385911234567",
+    "prosjecna_ocjena": 4.2,
+    "godina_upisa": 2021,
+    "naziv_studija": "Računarstvo",
+    "sveuciliste": "Sveučilište u Zagrebu"
+}
